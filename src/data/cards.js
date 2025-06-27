ebb2dc18aef5910ad4c37e482e2242703830b0de
Sean Xu <thisisseanxu@qq.com>
2025-06-27 18:53:53 +0800

    @docs 修改甜甜圈推车

--- src/data/cards.js
+++ src/data/cards.js
@@ -17,7 +17,7 @@ export const allCards = [
   // SSR 角色
   { id: 1102, name: '卷卷战士', rarity: RARITY.SSR, imageUrl: '/images/cards/1102.webp' },
   { id: 1105, name: '甜甜圈治疗师', rarity: RARITY.SSR, imageUrl: '/images/cards/1105.webp' },
-  { id: 1106, name: '甜甜圈双马尾', rarity: RARITY.SSR, imageUrl: '/images/cards/1106.webp' },
+  { id: 1106, name: '甜甜圈推车', rarity: RARITY.SSR, imageUrl: '/images/cards/1106.webp' },
   { id: 1108, name: '甜蜜迫击炮', rarity: RARITY.SSR, imageUrl: '/images/cards/1108.webp' },
   { id: 1109, name: '裱花千竹', rarity: RARITY.SSR, imageUrl: '/images/cards/1109.webp' },
   { id: 1201, name: '星星法师', rarity: RARITY.SSR, imageUrl: '/images/cards/1201.webp' },
