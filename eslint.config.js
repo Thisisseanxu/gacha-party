6943b54efc24da79a203faa4a96459da419a5bad
Sean Xu <thisisseanxu@qq.com>
2026-01-08 17:46:59 +0800

    chore: 为eslint添加支持gitignore的功能

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,25 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
+import { defineConfig } from 'eslint/config'
 import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 import pluginVueScopedCss from 'eslint-plugin-vue-scoped-css'
 
+import { includeIgnoreFile } from '@eslint/compat'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const gitignorePath = path.resolve(__dirname, '.gitignore')
+
 export default defineConfig([
+  includeIgnoreFile(gitignorePath),
+
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
-
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   {
     languageOptions: {
