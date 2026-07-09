d519de7e7f35149e4c47e4c8bceb01e4a4847073
Sean Xu <thisisseanxu@qq.com>
2026-07-09 20:32:20 +0100

    feat: 更新海风拂暑版本内容

--- src/data/cardPools.js
+++ src/data/cardPools.js
@@ -50,6 +50,74 @@ const SP_BASE_RATE = 0.0125 // SP 基础概率
  */
 const rawCardPoolsInOrder = [
   [
+    "140",
+    {
+      type: "限定",
+      name: "海风拂暑",
+      isAvailable: true,
+      imageUrl: "/images/cardpools/140.webp",
+      startTime: "2026-07-09 00:00:00",
+      finishTime: "2026-08-02 23:59:59",
+      tag_type: 2,
+      tag_name: "水流",
+      rates: {
+        [SP]: SP_BASE_RATE,
+        [SSR]: 0.06,
+        [SR]: 0.2,
+      },
+      rules: {
+        [SP]: {
+          pity: 60,
+          boostAfter: 40,
+          boost: 0.02,
+          UpTrigger: true,
+          SelectUpCards: true,
+          UpCards: [
+            "涓流花洒\t",
+            "绮彩泡泡"
+          ],
+        },
+        [SSR]: {},
+      },
+      cardNames: {
+        [SP]: [
+          "涓流花洒\t",
+          "绮彩泡泡"
+        ],
+        [SSR]: [
+          "水枪普拉斯",
+          "强力消防栓",
+          "功夫沙袋",
+          "竹子水车"
+        ],
+        [SR]: [
+          "珍珠射手",
+          "饼干骑士",
+          "劈咔灯泡",
+          "弹幕发射姬",
+          "电音射手",
+          "家用喷火枪",
+          "液氮喷射器",
+          "雪球投掷姬",
+          "梦游月兔",
+          "拳头礼盒",
+          "消防栓",
+          "浅睡千竹",
+          "贝斯战士"
+        ],
+        [R]: [
+          "铁头娃娃",
+          "纸杯千竹",
+          "转转咖啡杯",
+          "迷你风扇",
+          "闪耀灯球",
+          "火柴女孩"
+        ],
+      },
+    },
+  ],
+
+  [
     "1004",
     {
       type: "自选",
@@ -275,7 +343,7 @@ const rawCardPoolsInOrder = [
     {
       type: "限定",
       name: "霜汽补给",
-      isAvailable: true,
+      isAvailable: false,
       imageUrl: "/images/cardpools/137.webp",
       startTime: "2026-06-09 00:00:00",
       finishTime: "2026-07-05 23:59:59",
